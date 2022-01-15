eab3fc97bdedee19d9403ef9c2d93ad907180e38
nexuslrf <shootingstarlrf@hotmail.com>
2022-01-15 14:46:00 +0800

    finish nsvf_render, tested.

--- accelRF/render/nsvf_render.py
+++ accelRF/render/nsvf_render.py
@@ -1,8 +1,9 @@
-from typing import Callable, Dict, Optional, Tuple
+from typing import Callable, Dict, List, Optional, Tuple
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
 from torch import Tensor
-from accelRF.raysampler.utils import ndc_rays
 from accelRF.render.nerf_render import volumetric_rendering
 
 def masked_scatter(mask: torch.BoolTensor, x: Tensor):
@@ -12,6 +13,20 @@ def masked_scatter(mask: torch.BoolTensor, x: Tensor):
     return x.new_zeros(B, K, x.size(-1)).masked_scatter(
         mask.unsqueeze(-1).expand(B, K, x.size(-1)), x)
 
+def fill_in(shape, hits: Tensor, input: Tensor, initial=0.):
+    if torch.Size(shape) == input.shape:
+        return input   # shape is the same no need to fill
+
+    if isinstance(initial, torch.Tensor):
+        output = initial.expand(*shape)
+    else:
+        output = input.new_ones(*shape) * initial
+    if input is not None:
+        if len(shape) == 1:
+            output = output.masked_scatter(hits, input)
+        output = output.masked_scatter(hits.unsqueeze(-1).expand(*shape), input)
+    return output
+
 class NSVFRender(nn.Module):
     def __init__(
         self,
@@ -20,17 +35,29 @@ class NSVFRender(nn.Module):
         voxel_embedder: nn.Module,
         model: nn.Module,
         point_sampler: nn.Module,
+        early_stop_thres: float=0,
+        bg_color: Optional[nn.Module]=None,
         white_bkgd: bool=False,
-        chunk: int=1024*16
+        fwd_steps: int=4,
+        chunk: int=1024*64
     ):
+        '''
+        Args:
+            early_stop_thres: values in the form of $T$, where $T = \exp(-\sum_i\sigma_i\delta_i)$
+        '''
         super().__init__()
         self.pts_embedder = pts_embedder if pts_embedder is not None else nn.Identity()
         self.view_embedder = view_embedder if view_embedder is not None else nn.Identity()
         self.voxel_embedder = voxel_embedder
         self.model = model
         self.point_sampler = point_sampler
-        self.white_bkgd = white_bkgd
+        self.early_stop = (early_stop_thres != 0)
+        self.early_stop_thres = -np.log(early_stop_thres)
         self.chunk = chunk
+        self.fwd_steps = fwd_steps
+        self.bg_color = bg_color
+        self.white_bkgd = (white_bkgd and bg_color is None)
+
 
     def forward(self, rays_o: Tensor, rays_d: Tensor, vox_idx: Tensor, 
         t_near: Tensor, t_far: Tensor, ray_hits: Tensor):
@@ -40,29 +67,67 @@ class NSVFRender(nn.Module):
             vox_idx, t_near, t_far: [N_rays, max_hits]
             hits: [N_rays]
         '''
-        # TODO chunk forward
-        # TODO early stopping
-        # maintain the original rays
-        rays_o_ = rays_o.reshape(-1, 3).clone()
-        rays_d_ = rays_d.reshape(-1, 3).clone()
-        
-        if ray_hits.sum() > 0:
-            vox_idx, t_near, t_far = vox_idx[ray_hits], t_near[ray_hits], t_far[ray_hits] # [n_hits, max_hits]
-            rays_o, rays_d = rays_o[ray_hits], rays_d[ray_hits] # [n_hits, 3]
-            # point sampling
-            sample_out = self.point_sampler(rays_o, rays_d, vox_idx, t_near, t_far)
-            pts, p2v_idx, t_vals = sample_out # [n_hits, max_samples] + [3], [], []
+        n_rays = ray_hits.shape[0]
+        if n_rays > self.chunk:
+            ret = [self.forward(rays_o[ci:ci+self.chunk], rays_d[ci:ci+self.chunk]) 
+                        for ci in range(0, n_rays, self.chunk)]
+            ret = {
+                k: torch.cat([out[k] for out in ret], 0)
+                for k in ret[0]
+            }
+            return ret
+        else:
+            if ray_hits.sum() > 0:
+                vox_idx, t_near, t_far = vox_idx[ray_hits], t_near[ray_hits], t_far[ray_hits] # [n_hits, max_hits]
+                rays_o, rays_d = rays_o[ray_hits], rays_d[ray_hits] # [n_hits, 3]
+                # point sampling
+                sample_out = self.point_sampler(rays_o, rays_d, vox_idx, t_near, t_far)
+                pts, p2v_idx, t_vals, dists = sample_out # [n_hits, max_samples] + [3], [], []
+                mask_pts = p2v_idx.ne(-1)
+                max_samples= pts.shape[1]
+                n_pts_fwd, start_step = 0, 0
+                accum_log_T = 0
+                nn_out = []
+                for i in range(0, max_samples, self.fwd_steps):
+                    n_pts_step = mask_pts[:, i:i+self.fwd_steps].sum()
+                    if (i+self.fwd_steps >= max_samples) or (n_pts_fwd + n_pts_step > self.chunk):
+                        l, r = start_step, i+self.fwd_steps
+                        out, n_pts = self.step_forward(
+                            pts[:,l:r], p2v_idx[:,l:r], rays_d, mask_pts[:,l:r], dists[:,l:r])
+                        nn_out.append(out)
+                        start_step, n_pts_fwd = r, 0
+                        # oops, early_stop relies on free_energy (sigma * dist)
+                        if self.early_stop:
+                            with torch.no_grad():
+                                accum_log_T = accum_log_T + out['free_energy'].sum(1)
+                                mask_early_stop = accum_log_T > self.early_stop_thres
+                                mask_pts[mask_early_stop] = 0
+                    else:
+                        n_pts_fwd += n_pts_step
+                nn_out = {
+                    k: torch.cat([out[k] for out in nn_out], 1) for k in nn_out[0]
+                }
+                r_out = volumetric_rendering(nn_out['rgb'], nn_out['free_energy'], t_vals, 
+                            white_bkgd=self.white_bkgd, with_dist=True)   
+                ret = {'rgb': r_out['rgb'], 'disp': r_out['disp'], 'acc': r_out['acc']}
+                if 'feat_n2' in nn_out:
+                    ret['regz_term'] = (nn_out['feat_n2'] * r_out['weights']).sum(-1)[ray_hits]
+            else:
+                ret = {'rgb': None, 'disp': None, 'acc': None}
+            # TODO post-processing...
+            # fill_in un-hitted holes
+            ret['rgb'] = fill_in((n_rays, 3), ray_hits, ret['rgb'], 0.0)
+            ret['disp'] = fill_in((n_rays,), ray_hits, ret['disp'], 0.0)
+            ret['acc'] = fill_in((n_rays, ), ray_hits, ret['acc'], 0.0)
+            if self.bg_color is not None:
+                bg_rgb = self.bg_color(ret['rgb'])
+                missed = (1 - ret['acc'])
+                ret['rgb'] = ret['rgb'] + missed[...,None] * bg_rgb
             
-            nn_out, n_pts = self.mask_forward(pts, p2v_idx, rays_d)
-            
-            r_out = volumetric_rendering(
-                nn_out['rgb'], nn_out['sigma'], t_vals, white_bkgd=self.white_bkgd)
-        
-        ret = {'rgb': r_out['rgb'], 'disp': r_out['disp'], 'acc': r_out['acc']}
-        return r_out
+            return ret
 
-    def mask_forward(self, pts: Tensor, p2v_idx: Tensor, rays_d: Tensor):
-        mask_pts = p2v_idx.ne(-1) 
+    def step_forward(self, pts: Tensor, p2v_idx: Tensor, rays_d: Tensor, 
+        mask_pts: Tensor, dists: Optional[Tensor]=None):
         n_pts = mask_pts.sum()
         if n_pts == 0:
             return None, 0
@@ -76,8 +141,13 @@ class NSVFRender(nn.Module):
         )
 
         # scatter back...
-        out = {
-            'sigma': masked_scatter(nn_out['sigma']),
-            'rgb': masked_scatter(nn_out['rgb'])
-        }
+        out = {'rgb': masked_scatter(mask_pts, nn_out['rgb'])}
+        if dists is not None:
+            dists = dists[mask_pts]
+            free_energy = F.relu(nn_out['sigma']) * dists # activation is here!
+            out['free_energy'] = masked_scatter(mask_pts, free_energy)
+        else:
+            out['sigma'] = masked_scatter(mask_pts, nn_out['sigma'])
+        if 'feat_n2' in nn_out:
+            out['feat_n2'] = masked_scatter(mask_pts, nn_out['feat_n2'])
         return out, n_pts
--- example/nsvf/run_nsvf.py
+++ example/nsvf/run_nsvf.py
@@ -6,44 +6,47 @@ from torch.utils.data import DataLoader
 from accelRF.datasets import Blender
 from accelRF.raysampler import PerViewRaySampler, VoxIntersectRaySampler
 from accelRF.pointsampler import NSVFPointSampler
-from accelRF.models import PositionalEncoding, VoxelEncoding, NSVF_MLP
+from accelRF.models import PositionalEncoding, VoxelEncoding, NSVF_MLP, BackgroundField
 from accelRF.render.nsvf_render import NSVFRender
 from accelRF.rep.voxel_grid import VoxelGrid
+from accelRF.metrics.nsvf_loss import nsvf_loss
 # parameters
 datapath, scene = '/data/stu01/ruofan/nerf-experiment/data/nerf_synthetic/', 'lego'
 N_rand, N_iters, N_views = 1024, 100000, 4
-multires, multires_views = 10, 4
-N_samples, N_importance = 64, 128
-input_ch, input_ch_views = (2*multires+1)*32, (2*multires_views+1)*3
+multires, multires_views = 6, 4
+N_samples = 64, 128
+input_ch, input_ch_views = (2*multires+1)*32, (2*multires_views)*3
 voxel_size = 0.4
 step_size = 0.125
 embed_dim = 32
+loss_weights = {'rgb': 1., 'alpha': 1., 'reg_term': 1.}
 # prepare data
 dataset = Blender(datapath, scene, with_bbox=True)
 vox_grid = VoxelGrid(dataset.bbox, voxel_size).cuda()
 # ray sampler
 base_raysampler = PerViewRaySampler(dataset.get_sub_set('train'), N_rand*4, N_iters, N_views, precrop=False)
 vox_raysampler = VoxIntersectRaySampler(N_rand, base_raysampler, vox_grid)
-train_rayloader = DataLoader(vox_raysampler, num_workers=1, pin_memory=True)
+train_rayloader = DataLoader(vox_raysampler, num_workers=0) # vox_raysampler's N_workers==0, pin_mem==False
 # create model
 nsvf_render = NSVFRender( # TODO
     point_sampler=NSVFPointSampler(step_size),
-    embedder_pts=PositionalEncoding(N_freqs=multires),
-    embedder_views=PositionalEncoding(N_freqs=multires_views, include_input=False),
-    voxel_embedding=VoxelEncoding(vox_grid, embed_dim),
-    model=NSVF_MLP(in_ch_pts=input_ch, in_ch_dir=input_ch_views), # identify what changes we need to make.
+    pts_embedder=PositionalEncoding(N_freqs=multires, angular_enc=True),
+    view_embedder=PositionalEncoding(N_freqs=multires_views, include_input=False),
+    voxel_embedder=VoxelEncoding(vox_grid, embed_dim),
+    model=NSVF_MLP(in_ch_pts=input_ch, in_ch_dir=input_ch_views), 
+    bg_color=BackgroundField()
 ).cuda()
 # create optimizer
 optimizer  = torch.optim.Adam(nsvf_render.parameters(), lr=5e-4)
 # start training
 for i, ray_batch in enumerate(train_rayloader):
     # TODO
-    rays_o, rays_d = ray_batch['rays_o'][0].cuda(), ray_batch['rays_d'][0].cuda()
-    gt_rgb = ray_batch['gt_rgb'][0].cuda()
-    render_out = nsvf_render(rays_o, rays_d)
-    loss = ((gt_rgb - render_out['rgb'])**2).mean() # TODO, nsvf has more complicated loss
-    if 'rgb0' in render_out:
-        loss = loss + ((gt_rgb - render_out['rgb0'])**2).mean()
+    rays_o, rays_d = ray_batch['rays_o'][0], ray_batch['rays_d'][0]
+    vox_idx, t_near, t_far = ray_batch['vox_idx'][0], ray_batch['t_near'][0], ray_batch['t_far'][0]
+    hits, gt_rgb = ray_batch['hits'][0], ray_batch['gt_rgb'][0]
+    render_out = nsvf_render(rays_o, rays_d, vox_idx, t_near, t_far, hits)
+    loss, _ = nsvf_loss(render_out, gt_rgb, loss_weights)
     optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    if loss.grad_fn is not None:
+        loss.backward()
+        optimizer.step()

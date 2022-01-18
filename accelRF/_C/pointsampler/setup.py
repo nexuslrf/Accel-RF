b1b9549256a4865f87b33595291608cb1716d274
chensjtu <sjtuyangchen@icloud.com>
2022-01-19 04:52:05 +0800

    fix include not found for some environments.

--- accelRF/_C/pointsampler/setup.py
+++ accelRF/_C/pointsampler/setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
 import glob
+import os
 
 # specify src
+_cwd = os.getcwd()
 _ext_src_root = "./"
 _ext_sources = \
     glob.glob("{}/*.cpp".format(_ext_src_root)) + \
     glob.glob("{}/*.cu".format(_ext_src_root))
 _ext_headers = glob.glob("../include/*.h")
-
+_ext_include = os.path.join(_cwd, '../include')
 # Standalone package
 setup(
     name='accelRF_C_pointsampler',
@@ -17,8 +19,8 @@ setup(
             name='_ext',
             sources=_ext_sources,
             extra_compile_args={
-                'cxx': ['-O2', '-ffast-math', '-I../include'], 
-                'nvcc': ['-O2', '-I../include']})
+                'cxx': ['-O2', '-ffast-math', f'-I{_ext_include}'], 
+                'nvcc': ['-O2', f'-I{_ext_include}']})
     ],
     cmdclass={ 
         'build_ext' : BuildExtension 
